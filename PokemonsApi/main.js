1acf67f6810efccf2df8bc461d161e2db17d813b
llthanh714@live.com <llthanh714@live.com>
2022-10-06 09:33:12 +0700

    Add page mavigaetion

--- PokemonsApi/main.js
+++ PokemonsApi/main.js
@@ -1,8 +1,11 @@
 class Pokemon {
     constructor() {
-        this.nextPageUrl = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=100'
+        this.nextPageUrl = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=15'
         this.prePageUrl = ''
         this.lstPokemons = []
+        this.pokesTable = document.getElementById("poke-table")
+        this.preBtn = document.getElementById("pre-link")
+        this.nextBtn = document.getElementById("next-link")
     }
     async getPokemons(url) {
         const respones = await axios.get(url, {
@@ -25,9 +28,23 @@ class Pokemon {
         this.prePageUrl = previous
         this.lstPokemons = results
         this.showPokemonsToTable()
+
+        if (this.nextPageUrl == null){
+            this.nextBtn.parentElement.classList.add('disabled')
+        }
+        else{
+            this.nextBtn.parentElement.classList.remove('disabled')
+        }
+        if (this.prePageUrl == null){
+            this.preBtn.parentElement.classList.add('disabled')
+        }
+        else{
+            this.preBtn.parentElement.classList.remove('disabled')
+        }
     }
     showPokemonsToTable() {
         let htmls = ''
+        document.getElementById("poke-table").innerHTML = htmls
         this.lstPokemons.forEach((poke, index) => {
             htmls += `<tr>
                 <td>${index + 1}</td>
@@ -35,10 +52,22 @@ class Pokemon {
                 <td><a href="${poke.url}">Detail</a></td>
             </tr>`
         })
-        document.getElementById("poke-table").innerHTML = htmls
+        this.pokesTable.innerHTML = htmls
+    }
+    pageNavigation() {
+        this.nextBtn.onclick = () => {
+            this.getPokemons(this.nextPageUrl)
+                .then(respones => this.setDataFromApi(respones))
+        }
+        this.preBtn.onclick = () => {
+            this.getPokemons(this.prePageUrl)
+                .then(respones => this.setDataFromApi(respones))
+        }
     }
     init() {
-        this.getPokemons(this.nextPageUrl).then(respones => this.setDataFromApi(respones))
+        this.getPokemons(this.nextPageUrl)
+            .then(respones => this.setDataFromApi(respones))
+        this.pageNavigation()
     }
 };
 
